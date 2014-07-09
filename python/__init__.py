3303ab7d0e4f6f2b7b1dbc4d64c604b1baf4a464
Tobias Heinemann <tobias.heinemann@gmail.com>
2014-07-09 14:31:51 -0700

    Added __init__.py

---
+++ python/__init__.py
@@ -0,0 +1,3 @@
+from findmax import findmax
+from DSolveGC import DSolveGC
+from DSolveParallel import DSolveParallel
--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,13 @@
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
 from numpy import get_include
+from glob import glob
 
-setup (ext_modules = cythonize ("*.pyx"), include_dirs = [get_include ()])
+extensions = [Extension (ext[:-4], [ext]) for ext in glob ('*.pyx')]
 
+setup(
+  name = 'vlasov-sheet',
+  ext_modules = cythonize (extensions),
+  include_dirs = [get_include ()],
+)
